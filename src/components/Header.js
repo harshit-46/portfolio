2e310854bf8880ebd18dcfbc7ba86c2e7dceaa7d
Harshit Gupta <iamharshit999@gmail.com>
2024-07-10 13:01:35 +0530

    Changed

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='bg-black h-10 w-full'>This is a Header.</nav>
+        </div>
+    )
+}
+
+export default Header
