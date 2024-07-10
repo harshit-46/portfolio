12c89b3a15b2bf2c6cd0e0ba30e5845ebb1428db
Harshit Gupta <iamharshit999@gmail.com>
2024-07-10 17:16:57 +0530

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <nav className='bg-black h-10 w-full'>This is a Header.</nav>
+            <div className="flex">
+                <nav className="bg-black h-15 w-full text-white">This is a Header.</nav>
+                <button className="bg-sky-500 hover:bg-sky-700">This is a button</button>
+            </div>
         </div>
     )
 }
