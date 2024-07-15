616a98fdef39ffdbc68bdc7ff1ebd315a246971c
Harshit Gupta <iamharshit999@gmail.com>
2024-07-15 11:44:23 +0530

    Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div className="border-2 border-blue-500 justify-between">
-            <div className="flex">
-                <nav className="w-full bg-black text-white">
-                    <Link to="/home">Ⱨ₲.ĐɆV</Link>
-                    <div className="border-4 border-red-600 w-6/12 ml-25">
-                        <ul className='grid grid-cols-4 gap-1'>
+        <div className="">
+            <div className="flex bg-black">
+                <nav className="w-full bg-black text-base leading-6 font-semibold dark:text-white">
+                    <Link className="" to="/home">Ⱨ₲.ĐɆV</Link>
+                    <div className="w-6/12 ml-48">
+                        <ul className='grid grid-cols-4 gap-0'>
                             <li><Link to="/home">Home</Link></li>
                             <li><Link to="/tech">Tech</Link></li>
                             <li><Link to="/projects">Project</Link></li>
@@ -17,7 +17,7 @@ const Header = () => {
                         </ul>
                     </div>
                 </nav>
-                <a href="https://github.com/harshit-46" target='_blank' rel='noreferrer'><button><img src={IMAGES.imgone} alt="" height="40" width="40" className="mr-10 mt-3 mb-3"/></button></a>
+                <a href="https://github.com/harshit-46" target='_blank' rel='noreferrer'><button><img src={IMAGES.imgthree} alt="" height="32" width="32" className="mr-20 mt-4 mb-3"/></button></a>
             </div>
         </div>
     )
--- src/icons/images.js
+++ src/icons/images.js
@@ -1,6 +1,7 @@
 const IMAGES = {
     imgone: require("./ghlogo.png"),
-    imgtwo: require("./newgh.png")
+    imgtwo: require("./newgh.png"),
+    imgthree: require("./ghmain.PNG")
 };
 
 export default IMAGES
