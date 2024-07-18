9eb89dd57f5c9e8d684e88a79166a3b000ddf107
Harshit Gupta <iamharshit999@gmail.com>
2024-07-18 11:35:34 +0530

    Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ const Header = () => {
         <div className="">
             <div className="flex bg-black">
                 <nav className="w-full bg-black text-base leading-6 font-semibold dark:text-white">
-                    <Link className="" to="/home">Ⱨ₲.ĐɆV</Link>
-                    <div className="w-6/12 ml-48">
-                        <ul className='grid grid-cols-4 gap-0'>
+                    <Link className="inline-block" to="/home"><img src={IMAGES.imgfour} alt="" height="80" width="80" className="ml-20 mt-4" /></Link>
+                    <div className="w-6/12 inline-block border-2 border-red-500">
+                        <ul className='grid grid-cols-4'>
                             <li><Link to="/home">Home</Link></li>
                             <li><Link to="/tech">Tech</Link></li>
                             <li><Link to="/projects">Project</Link></li>
@@ -17,7 +17,7 @@ const Header = () => {
                         </ul>
                     </div>
                 </nav>
-                <a href="https://github.com/harshit-46" target='_blank' rel='noreferrer'><button><img src={IMAGES.imgthree} alt="" height="32" width="32" className="mr-20 mt-4 mb-3"/></button></a>
+                <a href="https://github.com/harshit-46" target='_blank' rel='noreferrer'><button><img src={IMAGES.imgthree} alt="" height="32" width="32" className="mr-20 mt-4 mb-3" /></button></a>
             </div>
         </div>
     )
--- src/icons/images.js
+++ src/icons/images.js
@@ -1,7 +1,8 @@
 const IMAGES = {
     imgone: require("./ghlogo.png"),
     imgtwo: require("./newgh.png"),
-    imgthree: require("./ghmain.PNG")
+    imgthree: require("./ghmain.PNG"),
+    imgfour: require("./AppLogo.png")
 };
 
 export default IMAGES
