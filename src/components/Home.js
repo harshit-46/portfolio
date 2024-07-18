220739d6aaa7343962a246c798f506ac543f69bc
Harshit Gupta <iamharshit999@gmail.com>
2024-07-18 18:55:29 +0530

    Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <div className="">
+            <div className="h-lvh w-full">
                 This is Home component.
             </div>
         </div>
