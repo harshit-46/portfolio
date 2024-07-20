a10486d14e2143ea9338e7a3735eb08eabeee5ab
Harshit Gupta <iamharshit999@gmail.com>
2024-07-21 00:11:21 +0530

    Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,25 @@
-import React from 'react'
-import IMAGES from '../icons/images'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import IMAGES from '../icons/images';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <>
-            <div className="flex fixed top-0 w-full" id="nav-root">
-                <nav className="w-full text-white" id="navbar">
-                    <Link className="inline-block" to="/welcome"><img src={IMAGES.imgfour} alt="" height="80" width="80" className="ml-24" /></Link>
-                    <div className="w-96 inline-block" id="nav-div">
-                        <ul className='grid grid-cols-5'>
-                            <li><Link to="/home">Home</Link></li>
-                            <li><Link to="/skills">Skills</Link></li>
-                            <li><Link to="/projects">Projects</Link></li>
-                            <li><Link to="/resume">Resume</Link></li>
-                            <li><Link to="/contact">Contact</Link></li>
-                        </ul>
-                    </div>
-                    <a href="https://github.com/harshit-46" target='_blank' rel='noreferrer'><button><img src={IMAGES.imgthree} alt="" height="32" width="32" className="" id="github-logo" /></button></a>
-                </nav>
-            </div>
-        </>
-    )
+        <div className="flex fixed top-0 w-full bg-gray-800" id="nav-root">
+            <nav className="w-full text-white flex items-center justify-between" id="navbar">
+                <Link to="/welcome" className="inline-block"><img src={IMAGES.imgfour} alt="" height="80" width="80" className="ml-4" /></Link>
+                <div className="inline-block border-1 border-red-500" id="nav-div">
+                    <ul className='flex space-x-4' id="nav-elemnts">
+                        <li><Link to="/home" className='px-4 py-2 rounded'>Home</Link></li>
+                        <li><Link to="/skills" className='px-4 py-2 rounded'>Skills</Link></li>
+                        <li><Link to="/projects" className='px-4 py-2 rounded'>Projects</Link></li>
+                        <li><Link to="/resume" className='px-4 py-2 rounded'>Resume</Link></li>
+                        <li><Link to="/contact" className='px-4 py-2 rounded'>Contact</Link></li>
+                    </ul>
+                </div>
+                <a href="https://github.com/harshit-46" target='_blank' rel='noreferrer'><button className="mr-4"><img src={IMAGES.imgthree} alt="" height="32" width="32" id="github-logo" /></button></a>
+            </nav>
+        </div>
+    );
 }
 
-export default Header
+export default Header;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import 'animate.css';
-import IMAGES from '../icons/images';
+import React from "react";
+import { Link } from "react-router-dom";
+import "animate.css";
+import IMAGES from "../icons/images";
 
 const Welcome = () => {
     return (
         <>
             <div id="welcome-main">
-                <h1 className="" id="welcome-text">Hello there, </h1> {/* animate__animated animate__bounceInUp */}
-                <h1>Welome to my</h1>
-                <h1>Portfolio</h1>
-                <Link to="/home"><img src={IMAGES.imgfive} alt="" height="40px" width="40px"/></Link>
+                <div id="welcome-text">
+                    <h1 id="hello">Hello there,</h1>
+                    <h1>Welome to my</h1>
+                    <h1>Portfolio</h1>
+                </div>
+                <div id="arrow-icon">
+                    <Link to="/home">
+                        <img src={IMAGES.imgfive} alt="" height="120px" width="120px" />
+                    </Link>
+                </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Welcome
+export default Welcome;
