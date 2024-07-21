af78a329219b35c2bdaca7bc3250e0058a0d1065
Harshit Gupta <iamharshit999@gmail.com>
2024-07-21 21:55:02 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,19 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className="flex fixed top-0 w-full bg-gray-800" id="nav-root">
-            <nav className="w-full text-white flex items-center justify-between" id="navbar">
-                <Link to="/welcome" className="inline-block"><img src={IMAGES.imgfour} alt="" height="80" width="80" className="ml-4" /></Link>
-                <div className="inline-block border-1 border-red-500" id="nav-div">
-                    <ul className='flex space-x-4' id="nav-elemnts">
-                        <li><Link to="/home" className='px-4 py-2 rounded'>Home</Link></li>
-                        <li><Link to="/skills" className='px-4 py-2 rounded'>Skills</Link></li>
-                        <li><Link to="/projects" className='px-4 py-2 rounded'>Projects</Link></li>
-                        <li><Link to="/resume" className='px-4 py-2 rounded'>Resume</Link></li>
-                        <li><Link to="/contact" className='px-4 py-2 rounded'>Contact</Link></li>
+        <div className="bg-gray-800">
+            <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between">
+                <Link to="/welcome" className="flex-shrink-0"><img src={IMAGES.imgfour} alt="" height="80" width="80" className="ml-2 sm:ml-0" /></Link>
+                <div className="hidden sm:block">
+                    <ul className="flex space-x-4">
+                        <li><Link to="/home" className="text-white px-3 py-2 rounded hover:bg-gray-700">Home</Link></li>
+                        <li><Link to="/skills" className="text-white px-3 py-2 rounded hover:bg-gray-700">Skills</Link></li>
+                        <li><Link to="/projects" className="text-white px-3 py-2 rounded hover:bg-gray-700">Projects</Link></li>
+                        <li><Link to="/resume" className="text-white px-3 py-2 rounded hover:bg-gray-700">Resume</Link></li>
+                        <li><Link to="/contact" className="text-white px-3 py-2 rounded hover:bg-gray-700">Contact</Link></li>
                     </ul>
                 </div>
-                <a href="https://github.com/harshit-46" target='_blank' rel='noreferrer'><button className="mr-4"><img src={IMAGES.imgthree} alt="" height="32" width="32" id="github-logo" /></button></a>
+                <a href="https://github.com/harshit-46" target="_blank" rel="noreferrer"><img src={IMAGES.imgthree} alt="" height="32" width="32" className="ml-4 sm:ml-2" /></a>
             </nav>
         </div>
     );
