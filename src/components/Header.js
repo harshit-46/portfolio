3735875bba90c077c6550ce55ec05edf692acb0a
Harshit Gupta <iamharshit999@gmail.com>
2024-07-28 16:06:58 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div className="bg-gray-800">
+        <div className="bg-black">
             <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex items-center justify-between">
                 <Link to="/welcome" className="flex-shrink-0"><img src={IMAGES.imgfour} alt="" height="80" width="80" className="ml-2 sm:ml-0" /></Link>
                 <div className="hidden sm:block">
